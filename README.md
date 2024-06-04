f6506866acf20c7e58187e041a053b12d88054c1
carolacevedo <carolacevedobr@icloud.com>
2024-06-04 20:34:35 -0300

    Implementacao desafio iphone

---
+++ README.md
@@ -0,0 +1,23 @@
+# 📱 Desafio de entrega de classes do iPhone
+
+## 📋 Descrição
+Este repósitorio contem a implemetação do diagrama uml proposto para o desafio.
+apresentamos o diagrama em pdf e png na pasta src/docs e o código em Java na pasta src/iphone
+
+## 📊 Diagrama UML
+<p align="center">
+  <img src="docs/carol-acevedo-diagrama.png" alt="Diagrama de Classes">
+</p>
+
+
+Nesta seção, apresentamos uma explicação detalhada de cada classe representada no diagrama de classe do projeto. Cada classe desempenha um papel específico e contribui para a funcionalidade global do sistema.
+
+### `iPhone` 
+
+Temos a classe  `iPhone`  que é a classe principal que é uma representação do dispositivo IPhome.
+Ela implementa as interfaces propostas:
+### `ReprodutorMusical`
+### `AparelhoTelefonico`
+### `NavegadorInternet`. 
+
+
---
+++ src/iphone/IPhone.java
@@ -0,0 +1,56 @@
+package iphone;
+
+public class IPhone implements AparelhoTelefonico, NavegadorNaInternet,ReprodutorMusical{
+
+    @Override
+    public void ligar(String numero) {
+        // Sua implementação aqui!
+    }
+
+    @Override
+    public void atender() {
+        // Sua implementação aqui!
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        // Sua implementação aqui!
+
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        // Sua implementação aqui!
+
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        // Sua implementação aqui!
+
+    }
+
+    @Override
+    public void atualizarPagina() {
+        // Sua implementação aqui!
+
+    }
+
+    @Override
+    public void tocar() {
+        // Sua implementação aqui!
+
+    }
+
+    @Override
+    public void pausar() {
+        // Sua implementação aqui!
+
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        // Sua implementação aqui!
+
+    }
+}
